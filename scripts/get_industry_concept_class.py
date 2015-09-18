a0ebd37ffc1d9fa071f5dc3347cda2d874a510cd
destiny1020 <destiny.jiang@gmail.com>
2015-09-19 01:11:28 +0800

    add concept & industry script

---
+++ scripts/get_industry_concept_class.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+import tushare as ts
+
+engine = create_engine('mysql+pymysql://root:adobe@127.0.0.1/tushare?charset=utf8')
+
+# industry
+df = ts.get_industry_classified()
+df.to_sql('industry_data', engine)
+
+# concept
+df = ts.get_concept_classified()
+df.to_sql('concept_data', engine)
+
+# area
+# df = ts.get_area_classified()
+# df.to_sql('area_data', engine)
