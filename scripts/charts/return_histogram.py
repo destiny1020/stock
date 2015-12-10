0149ef80891b03c1d888e06622cb9f84218d067c
destiny1020 <destiny.jiang@gmail.com>
2015-12-11 01:39:53 +0800

    tweak the histogram chart style

--- scripts/charts/return_histogram.py
+++ scripts/charts/return_histogram.py
@@ -1,5 +1,10 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('ggplot')
+pd.options.display.mpl_style = 'default'
 
 # retrieve the current data
 engine = create_engine('mysql+pymysql://root:adobe@127.0.0.1/tushare?charset=utf8')
@@ -9,9 +14,9 @@ df = pd.read_sql_query('select * from data_daily where code = ' + '000001' + ' o
 # percentage change
 change_series = df['p_change']
 
-change_series_chart = change_series.hist(bins=50, figsize=(12,8))
+change_series_chart = change_series.hist(bins=50)
 
 print type(change_series_chart)
 
 fig = change_series_chart.get_figure()
-fig.savefig("output.png")
+fig.savefig("output2.png")
